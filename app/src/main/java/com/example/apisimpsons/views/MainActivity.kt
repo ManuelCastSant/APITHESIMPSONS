5c5f9a30eaf1a3f41aa15d6c7b15c2a30e216312
Manuel Enrique Castañeda Santana <mike.nike335@gmail.com>
2023-12-28 01:35:53 -0600

    Agregar

--- app/src/main/java/com/example/apisimpsons/views/MainActivity.kt
+++ app/src/main/java/com/example/apisimpsons/views/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.example.apisimpsons.views
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.apisimpsons.R
+import com.example.apisimpsons.databinding.ActivityMainBinding
+import com.example.apisimpsons.viewmodels.MainViewModel
+import com.example.apisimpsons.views.adapters.PersonajeAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: PersonajeAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        setupRecyclerView()
+
+        viewModel.obtenerPersonajes()
+
+        viewModel.listaPersonajes.observe(this) {
+            adapter.listaPersonajes = it
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.tilBuscar.setEndIconOnClickListener {
+            ocultarTeclado()
+            if (binding.tietBuscar.text.toString() == "") {
+                viewModel.obtenerPersonajes()
+            } else {
+                viewModel.obtenerPersonaje(binding.tietBuscar.text.toString().trim())
+            }
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvPersonajes.layoutManager = GridLayoutManager(this, 3)
+        adapter = PersonajeAdapter(this, arrayListOf())
+        binding.rvPersonajes.adapter = adapter
+    }
+
+    private fun ocultarTeclado() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
